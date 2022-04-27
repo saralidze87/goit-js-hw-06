59794c3eadd21688e2a1a394bb16d830aa89c6db
Oksana Saralidze <saralidze.ok@gmail.com>
2022-04-27 22:23:37 +0300

    task-1

---
+++ src/js/task-01.js
@@ -0,0 +1,4 @@
+const catEl = document.querySelectorAll('li.item');
+console.log('Number of categories:', catEl.length);
+const catListEl = document.querySelectorAll('li.item.h2');
+console.log('Category:', catListEl);
