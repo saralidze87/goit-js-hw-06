a5dfe79f119bc2a1f18a589ec18c8e4316ccd078
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 13:37:56 +0300

    add task3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const galleryListRef = document.querySelector('.gallery');
+
+const markup = images
+  .map(({ url, alt }) => {
+    return `<li class="gallery__item"><img src=${url} alt="${alt}"></li>`;
+  })
+  .join('');
+
+galleryListRef.insertAdjacentHTML('afterbegin', markup);
+console.log(galleryListRef);
