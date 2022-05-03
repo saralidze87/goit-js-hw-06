71d39c14d5609b59f66533007b22f7a8d0e6fe10
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 15:42:23 +0300

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  if (email.length === 0 || password.length === 0) {
+    alert('Все поля должны быть заполнены!!!');
+  } else {
+    const formData = { email, password };
+    console.log(formData);
+  }
+  form.reset();
+}
