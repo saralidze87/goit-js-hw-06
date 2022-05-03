78b9647025a586c6d4f6764e07b4fd58e10af686
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 12:02:46 +0300

    finish task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,19 +1,18 @@
-const catEl = document.querySelectorAll('li.item');
-console.log('Number of categories:', catEl.length);
+const catEl = document.getElementById('categories');
+const catListEl = catEl.querySelectorAll('.item');
+const catValue = catListEl.length;
+console.log('Number of categories:', catValue);
 
-const catListEl = document.querySelectorAll('#categories>li');
-catListEl.forEach((el,) => {
-    
-    console.log('Category:', catListEl, 'Elements:', catListEl.length);
-   })
+for (let i = 0; i < catValue; i += 1) {
+  const catTitle = catListEl[i].querySelector('h2');
+  const catName = catTitle.textContent;
+  console.log(`Category: ${catName}`);
 
-// function categoryNames() {
-//     const catListEl = document.querySelectorAll('h2');
-//     for(const name in catListEl) {
-//         console.log(name);
-//         console.log(catListEl[name]);
-//     };
-// }
-//     // catListEl.forEach(catListEl => 
-//     //     console.log('Category: catListEl.textContent'))};
-//         categoryNames();
+  const catElRef = catListEl[i].querySelectorAll('li');
+  const catValueRef = catElRef.length;
+  console.log(`Elements: ${catValueRef}`);
+}
+
+
+
+
