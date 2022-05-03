f591428d958be28dd9b5e36c92d919081b1c7b64
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 12:48:54 +0300

    add task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listOfIngredientsRef = document.querySelector('[id="ingredients"]');
+
+const arrOfIngredientItems = ingredients.map(item => {
+  const tagLi = document.createElement('li');
+  tagLi.classList.add('item');
+  tagLi.textContent = item;
+
+  return tagLi;
+});
+
+listOfIngredientsRef.append(...arrOfIngredientItems);
+console.log(listOfIngredientsRef);
