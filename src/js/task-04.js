026ba7dc65a112ee6444133676cc10cb5599a021
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 13:48:49 +0300

    add task 4

---
+++ src/js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const counterValueRef = document.getElementById('value');
+
+decrementBtn.addEventListener('click', minusOne);
+incrementBtn.addEventListener('click', plusOne);
+
+function minusOne() {
+  counterValue -= 1;
+  counterValueRef.textContent = counterValue;
+}
+// console.log(counterValue - 1);
+function plusOne() {
+  counterValue += 1;
+  counterValueRef.textContent = counterValue;
+}
+// console.log(counterValue + 1);
