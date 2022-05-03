4e7e78b3933b907990bb742bb7c0fc92e739c3f3
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 11:27:29 +0300

    update task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,19 @@
 const catEl = document.querySelectorAll('li.item');
 console.log('Number of categories:', catEl.length);
-const catListEl = document.querySelectorAll('li.item.h2');
-console.log('Category:', catListEl);
+
+const catListEl = document.querySelectorAll('#categories>li');
+catListEl.forEach((el,) => {
+    
+    console.log('Category:', catListEl, 'Elements:', catListEl.length);
+   })
+
+// function categoryNames() {
+//     const catListEl = document.querySelectorAll('h2');
+//     for(const name in catListEl) {
+//         console.log(name);
+//         console.log(catListEl[name]);
+//     };
+// }
+//     // catListEl.forEach(catListEl => 
+//     //     console.log('Category: catListEl.textContent'))};
+//         categoryNames();
