25624337f151836b55148ee1a106cbe3ead378f6
Oksana Saralidze <saralidze.ok@gmail.com>
2022-05-03 15:50:48 +0300

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const elBody = document.querySelector('body');
+const elBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+elBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor(event) {
+  const generatedColor = getRandomHexColor();
+  elBody.style.backgroundColor = generatedColor;
+  colorName.textContent = generatedColor;
+}
